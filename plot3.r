1744cce5e4de80b0b212b607e589ce0d8f40ae98
ValiantDoge <agnelofernandes475@gmail.com>
2024-04-29 03:28:23 +0530

    Plot 3 done

---
+++ plot3.r
@@ -0,0 +1,39 @@
+#Read the Dataset as a table
+power_cons <- read.table("household_power_consumption.txt",
+                         skip = "1",
+                         sep = ";")
+#Naming the columns
+names(power_cons) <- c("Date", "Time", "Global_active_power",
+                       "Global_reactive_power", "Voltage", "Global_intensity",
+                       "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+#Converting the Date column to Date format
+power_cons$Date <- as.Date(power_cons$Date, "%d/%m/%Y")
+
+#Subset the data for the dates 1/2/2007 and 2/2/2007
+power_cons_subset <- subset(power_cons,
+                            Date >= as.Date("1/2/2007", "%d/%m/%Y")
+                            & Date <= as.Date("2/2/2007", "%d/%m/%Y"))
+
+#Convert the Date and Time columns to a single datetime object
+#Use paste to concat the Date and Time columns
+date_time <- strptime(paste(power_cons_subset$Date,
+                            power_cons_subset$Time),
+                      "%Y-%m-%d %H:%M:%S")
+
+#Plotting the line graph for the three sub metering variables
+#Save the plot as a PNG file, setting the Device before plotting so that the legend doesnt cramp
+png("plot3.png", width=480, height=480)
+
+plot(date_time, power_cons_subset$Sub_metering_1,
+     type = "n", xlab = "", ylab = "Energy sub metering")
+lines(date_time, power_cons_subset$Sub_metering_1)
+lines(date_time, power_cons_subset$Sub_metering_2, col = "red")
+lines(date_time, power_cons_subset$Sub_metering_3, col = "blue")
+
+#Add a legend to the plot
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=1,
+       col=c("black", "red", "blue"))
+
+dev.off()
